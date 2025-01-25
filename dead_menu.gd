02bed18caaaad8dcdb4d395d4237918a957548d1
fallenatlas <tiago.a.p.antunes@gmail.com>
2025-01-25 19:34:13 +0000

    feat: finish dead reset

--- dead_menu.gd
+++ dead_menu.gd
@@ -5,6 +5,7 @@ extends Control
 @export var terrain_manager : Node2D
 @export var player : CharacterBody2D
 @export var camera : Camera2D
+@export var animation : AnimationPlayer
 
 var kill_count : int = 0
 
@@ -12,6 +13,7 @@ var kill_count : int = 0
 func _ready() -> void:
 	Events.enemy_kill.connect(_on_enemy_kill)
 	Events.dead.connect(_on_player_dead)
+	animation.play("fade_out")
 
 func _on_enemy_kill() -> void:
 	kill_count += 1
@@ -24,12 +26,15 @@ func _on_player_dead() -> void:
 
 func _on_restart_pressed() -> void:
 	#animation.start
+	animation.play("fade_in")
+	await animation.animation_finished
+	
 	self.hide()
 	#when end:
 	terrain_manager.start_map()
 	player.reset()
 	camera.limit_bottom = 352
-	#animation.start
+	animation.play("fade_out")
 	kill_count = 0
 	
 	
